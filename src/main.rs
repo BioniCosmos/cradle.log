349f99e42494362adbe2187f2b6ab7fa90c6d16f
Hane Velarith <41363844+BioniCosmos@users.noreply.github.com>
2026-06-30 15:50:08 +0800

    Implement query, update, deletion endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use std::sync::Arc;
 
-use axum::{Json, Router, extract::State, http::StatusCode, routing};
+use axum::{
+    Json, Router,
+    extract::{Path, State},
+    http::StatusCode,
+    routing,
+};
 use reqwest::{Client, header};
 use serde::{Deserialize, Serialize};
-use sqlx::{PgPool, query};
+use sqlx::{PgPool, query, query_as};
 use time::OffsetDateTime;
 use tokio::{net::TcpListener, task::JoinHandle};
 use tower_http::trace::TraceLayer;
-use tracing::info;
+use tracing::{error, info};
 use tracing_subscriber::{EnvFilter, fmt, prelude::*};
 
 struct AppState {
@@ -29,7 +34,14 @@ async fn main() {
     let state = Arc::new(AppState { pool });
 
     let router = Router::new()
-        .route("/api/journal", routing::post(create_journal))
+        .route(
+            "/api/journal",
+            routing::post(create_journal).get(query_journals),
+        )
+        .route(
+            "/api/journal/{id}",
+            routing::patch(update_journal).delete(delete_journal),
+        )
         .with_state(state)
         .layer(TraceLayer::new_for_http());
     let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
@@ -64,6 +76,22 @@ struct ClinicalEntities {
     diseases: Vec<String>,
     symptoms: Vec<String>,
     suspected_diseases: Vec<String>,
+}
+
+#[derive(Serialize)]
+struct Disease {
+    id: i64,
+    diseases: Vec<String>,
+    symptoms: Vec<String>,
+
+    #[serde(with = "time::serde::rfc3339")]
+    creation_time: OffsetDateTime,
+}
+
+#[derive(Deserialize)]
+struct JournalUpdateParams {
+    diseases: Option<Vec<String>>,
+    symptoms: Option<Vec<String>>,
 }
 
 async fn create_journal(
@@ -109,7 +137,7 @@ async fn create_journal(
                 &result.diseases
             },
             &result.symptoms,
-            OffsetDateTime::now_local()?
+            OffsetDateTime::now_local()?,
         )
         .execute(&state.pool)
         .await?;
@@ -117,4 +145,58 @@ async fn create_journal(
         Ok(())
     }) as JoinHandle<anyhow::Result<()>>;
     StatusCode::NO_CONTENT
+}
+
+async fn query_journals(
+    State(state): State<Arc<AppState>>,
+) -> Result<Json<Vec<Disease>>, StatusCode> {
+    match query_as!(
+        Disease,
+        "SELECT * FROM diseases ORDER BY creation_time DESC",
+    )
+    .fetch_all(&state.pool)
+    .await
+    {
+        Ok(diseases) => Ok(Json(diseases)),
+        Err(e) => {
+            error!(?e);
+            Err(StatusCode::INTERNAL_SERVER_ERROR)
+        }
+    }
+}
+
+async fn update_journal(
+    State(state): State<Arc<AppState>>,
+    Path(id): Path<i64>,
+    Json(payload): Json<JournalUpdateParams>,
+) -> StatusCode {
+    match query!(
+        "UPDATE diseases SET diseases = COALESCE($1, diseases), symptoms = COALESCE($2, symptoms) WHERE id = $3",
+        payload.diseases.as_deref(),
+        payload.symptoms.as_deref(),
+        id,
+    )
+    .execute(&state.pool)
+    .await {
+        Ok(result) if result.rows_affected() > 0 => StatusCode::NO_CONTENT,
+        Ok(_) => StatusCode::NOT_FOUND,
+        Err(e) => {
+            error!(?e);
+            StatusCode::INTERNAL_SERVER_ERROR
+        }
+    }
+}
+
+async fn delete_journal(State(state): State<Arc<AppState>>, Path(id): Path<i64>) -> StatusCode {
+    match query!("DELETE FROM diseases WHERE id = $1", id)
+        .execute(&state.pool)
+        .await
+    {
+        Ok(result) if result.rows_affected() > 0 => StatusCode::NO_CONTENT,
+        Ok(_) => StatusCode::NOT_FOUND,
+        Err(e) => {
+            error!(?e);
+            StatusCode::INTERNAL_SERVER_ERROR
+        }
+    }
 }
